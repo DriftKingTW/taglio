0e04a6a10f44375fd8f9434c4ee6deabfcb3cef1
DriftKingTW <driftkingtw@gmail.com>
2021-05-30 01:41:37 +0800

    Fix initial aspect ratio not matching the width and height

--- pages/index.vue
+++ pages/index.vue
@@ -164,7 +164,7 @@ export default Vue.extend({
       ],
       currentTemplate: 'Default',
       showOverlay: false,
-      aspectRatio: 16 / 9,
+      aspectRatio: WIDTH / HEIGHT,
       imgWidth: WIDTH,
       imgHeight: HEIGHT,
       image: defaultImage,
